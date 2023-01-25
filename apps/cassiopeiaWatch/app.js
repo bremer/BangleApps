fd32149d4a0c1c68c9cd8cb5256c0e74c7052dbc
mbremer <m.bremer@neusta.de>
2023-01-25 20:39:08 +0100

    [cassiopeiaWatch] rearange elements

--- apps/cassiopeiaWatch/app.js
+++ apps/cassiopeiaWatch/app.js
@@ -13,7 +13,7 @@ function bigThenSmall(big, small, x, y) {
 }
 
 function getBackgroundImage() {
-  return require("heatshrink").decompress(atob("2GwwkEIf4A/AH4A/AH4AmkdEocQgFEoUAgM0onyAYYLBkEC+lPmAGBC4MgggEBoUEDQIGBkBWwgVEkMjiAEBocAh9CiUyAYZXCmnyiYPBC4M0oJTCKgIDBklEMwIAvig6CgEDp9EBANBBgQDCKARZBgICBAIIaBK400+lAK+EkJQZBBIoVEmUQAYZXBkVEAgP0mBXJMgMkPgYAtmiKDklAmkwgdEolCAYZXBka8BK4chmlBggPBoBXBgVEgdDV+pVBAwUj+g9BAYavHolDkCvFKoJZBCIIAudgIDBdAKnDgCVDAYSoBokgCIMgAIJTDAYcUDwQMCAFr6BkMjidPJ4UfkMTAYhXBkE0+QGEK480+DQCK976BolPj5FBdIMTSgMwAYZQCgX0p4GDKYTHCK4P0BgMkoJXwAH4A/AH4A/AH4AaogAzGrtAKwUEK/5YVK2pXfD8A3/K/4A/K9lAgEEK/BgaKwIABK+47BEbRX6HLZWCghX2SLbKBK/A6CHLJWCZjRXcSLh0cK7jpDKzIjELN5PEdLisED7RXZVwaPZoAaCEIRXyVwaxbEIhXxVwYADWTRX0V45X/K6A4FKzIeDDrRXaVwYkbV2BXJSDauxK4rnCVzgfBK15XIVzitwK5IjcK+5WdAGhXFSGRXkIv5XWAH5X/K/4A/K/5X/K/4A/K9cAAH4A/AFz/uAH4A/AH4AaoEAggEGAH5WOAAIEFAH4ANKIQAFWH5XXWH4ANoBX/K7RbGJX5XQohX/K6sEK4MELwgA/ABhXCKoJaCLwQA/AB5XBAQJD/WCcAolAV34ASKYQDEAH6wSgiu/WC6u/WC6u/WDBX/WC5B/WCxA/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4ApgAA/AH4AuZ8AYXoEEHT5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K9w5YK8QDCoAhXC4NAK/AdCgEAXChuBAATSaK7Y6DEAQiUK4geCK+o9BAwbuaK+lAAoQGBgiv/K56sCDoJXYCwIBDK/4eTDARX0DYQ8BK7EAAAhX0LQZXCAwRX/V5wABKgJaDDygBCHrJXcDoYBCV/5XSAYIGCK/5XSH4o8YADRXfLASuTK/4A4K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5XmOsBX3G7BX9gEAWDxX1VwJYfK+pWCAAJX/VypYeK+hTCA"));
+  return require("heatshrink").decompress(atob("2GwwkEIf4A/AH4A/AH4AmkdEocQgFEoUAgM0onyAYYLBkEC+lPmAGBC4MgggEBoUEDQIGBkBWwgVEkMjiAEBocAh9CiUyAYZXCmnyiYPBC4M0oJTCKgIDBklEMwIAvig6CgEDp9EBANBBgQDCKARZBgICBAIIaBK400+lAK+EkJQZBBIoVEmUQAYZXBkVEAgP0mBXJMgMkPgYAtmiKDklAmkwgdEolCAYZXBka8BK4chmlBggPBoBXBgVEgdDV+pVBAwUj+g9BAYavHolDkCvFKoJZBCIIAudgIDBdAKnDgCVDAYSoBokgCIMgAIJTDAYcUDwQMCAFr6BkMjidPJ4UfkMTAYhXBkE0+QGEK480+DQCK976BolPj5FBdIMTSgMwAYZQCgX0p4GDKYTHCK4P0BgMkoJXwAH4A/AH4A/AH4AaogAzGrtAKwUEK/5YVK2pXfD8A3/K/4A/K/5XoMH5XWoBXZDQMEAgxXygBXYKIIbCAghXyGzQaCAAqwsJwqOCGrBXIWFgsFRrRyCK/A7DEC4bDLYxXwGjZzEK+o6DghXaDYICBaTZXXRoo2XK4RVBLQR6ZK6yKDAAY3ZK4ICBKlavNHS7KDPYRZtfoqyDG7AcCAYhXyVwYjaOYKuvK5I3aZoauuK4o4DEjiuwK5A3cD75XaErwfeK6w2goBWvK4zlvK85F/K6wA/K/5X/AH5X/K/5X/AH5XrgAA/AH4Auf9wA/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AEEAAH4A/AFzNeDTNAgg6dK/5X/K/5XSgBeTK8CTaDYpWBgBDSK8VAK7lAK7QDCDwI5UGoY2TK5o4TGgYYCHqxXEHChXHDyxXCDIgBDAGAyCPAaRTK4gcBAwav4HChQBoAFCAwMEDyyNBAIRXZghACIIY4RVgQfBK7JZCZSpXFDQY/BLYRXucwTpWK5IfBK6YaCLYJX4EYpXULQZXCAwRXSCwVAH4xXXO4SvVC4Q7CK6qviK64fDAIRX7dCYECAYIGCK6gAdJ4h6CHKRNGS4ZWwK5DoTNYwdBV2JXEIAI5UfuRXOc6pX/K/5XYACxX/K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5X/K/5XtOrxX5HLRX7gEADzpX2VwJYdK+5WCAAJX/VypYcK+xTCA"));
 }
 
 let settings = storage.readJSON("cassiopeiaWatch.settings.json", true) || {};
@@ -39,22 +39,21 @@ function clearIntervals() {
 
 function drawClock() {
   g.setFont("7x11Numeric7Seg", 4);
-  g.clearRect(42, 76, 171, 120);
+  g.clearRect(25, 65, 171, 120);
   g.setColor(0, 255, 255);
-  //g.drawRect(80, 57, 170, 96);
-  g.fillRect(42, 76, 171, 120);
+  g.fillRect(25, 65, 171, 120);
   g.setColor(0, 0, 0);
-  g.drawString(require("locale").time(new Date(), 1), 42, 76);
+  g.drawString(require("locale").time(new Date(), 1), 25, 65);
 }
 
 function drawDate() {
   g.setFont("8x12", 2);
   g.drawString(require("locale").dow(new Date(), 2).toUpperCase(), 18, 126);
   g.setFont("8x12");
-  g.drawString(require("locale").month(new Date(), 2).toUpperCase(), 43, 160);
-  g.setFont("8x12", 2);
+  g.drawString(require("locale").month(new Date(), 2).toUpperCase(), 1, 152);
+  g.setFont("7x11Numeric7Seg", 2);
   const time = new Date().getDate();
-  g.drawString(time < 10 ? "0" + time : time, 18, 150);
+  g.drawString(time < 10 ? "0" + time : time, 30, 150);
 }
 
 function drawBattery() {
@@ -88,13 +87,16 @@ function draw() {
   g.setColor(0, 0, 0);
 
   g.setFontAlign(0, -1);
-  g.setFont("8x12", 2);
-  g.drawString(getTemperature(), 155, 132);
-  g.drawString(Math.round(Bangle.getHealthStatus("last").bpm), 50, 55);
+  g.setFont("7x11Numeric7Seg", 2);
+  g.drawString(getTemperature(), 150, 130);
+
+  // TODO info line
+  g.setFont("7x11Numeric7Seg", 2);
+  g.drawString(Math.round(Bangle.getHealthStatus("last").bpm), 30, 21);
 
   g.setFont("7x11Numeric7Seg", 2);
   g.setFontAlign(-1, -1);
-  g.drawString(getSteps(), 20, 21);
+  g.drawString(getSteps(), 65, 150);
 
   g.setFontAlign(-1, -1);
   drawClock();
